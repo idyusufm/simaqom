c05492be15bc93bf3a0ae735c0e7821d848c0f13
idyusufm <id.yusufm@gmail.com>
2026-09-16 15:04:26 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,3 +15,7 @@ export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const googleProvider = new GoogleAuthProvider()
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+})
